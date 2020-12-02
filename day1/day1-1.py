8a66b5aacef6bdbf79fcef1ff5965b3b387a96ee
unknown <mrazzakov@gmail.com>
2020-12-02 16:02:01 -0600

    completed day 1

--- day1/day1-1.py
+++ day1/day1-1.py
@@ -16,7 +16,7 @@
 # 1721
 # 979
 # 366
-# 299
+# 299 
 # 675
 # 1456
 # In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
